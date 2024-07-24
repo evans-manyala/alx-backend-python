79c3eb2c0c6f09bda54d836c67d65762eb1aa565
Evans Manyala <evans.manyala@gmail.com>
2024-07-24 15:22:38 +0300

    Fixing doc style documentation

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 """
-Function’s parameters & return values
-with the appropriate types
+Function’s parameters and return values
+annotated with the appropriate types.
 """
-from typing import List, Tuple, Iterable, Sequence
+from typing import List, Tuple, Sequence
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: List[Sequence[str]]) -> List[Tuple[str, int]]:
     """
-    Create a list of tuples where each
-    tuple contains a string from the input list & its length.
+    Create a list of tuples where each tuple contains
+    a string from the input list and its length.
 
     Args:
-        lst (List[str]): A list of strings.
+        lst (List[Sequence[str]]): A list of sequences where
+        each sequence is expected to be a string.
 
     Returns:
-        List[Tuple[str, int]]: A list of tuples where each
-        tuple contains a string & its length.
+        List[Tuple[str, int]]: A list of tuples where
+        each tuple contains a string and its length.
     """
     return [(i, len(i)) for i in lst]

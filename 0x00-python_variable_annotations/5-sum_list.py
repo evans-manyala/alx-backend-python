178c1a6ed71381ee62d8bc0fd9c2e85c84907d05
Evans Manyala <evans.manyala@gmail.com>
2024-07-24 11:17:42 +0300

    Adding missing list keyword

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,7 +3,7 @@
 from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Calculates the sum of a list of floats.
 
     Args:

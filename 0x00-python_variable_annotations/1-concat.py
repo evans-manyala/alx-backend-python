26dc725d5b05f3c7bda0c58185b80e39cc17e52d
Evans Manyala <evans.manyala@gmail.com>
2024-07-24 10:04:30 +0300

    Adding concat code logic

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Concatenates two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings.
+
+    Args:
+        str1: The first string.
+        str2: The second string.
+
+    Returns:
+        The concatenation of str1 and str2.
+    """
+    return str1 + str2

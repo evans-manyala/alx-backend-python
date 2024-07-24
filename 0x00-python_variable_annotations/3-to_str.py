4dc5ea0b3b9a69ccf1197b4504a51917b9b19364
Evans Manyala <evans.manyala@gmail.com>
2024-07-24 10:57:04 +0300

    Fixing pycodestyle errors

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -6,12 +6,12 @@ Converts a float to a string representation
 
 def to_str(n: float) -> str:
     """Converts a float to a string representation.
-    
+
     Args:
     n: The input float.
-    
+
     Returns:
     The string representation of the float.
     """
-    
+
     return str(n)

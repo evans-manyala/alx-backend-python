30a5e712016a94321220e867e642a7e2d9238567
Evans Manyala <evans.manyala@gmail.com>
2024-07-24 09:53:40 +0300

    Adding code logic

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Funtion Adds two float numbers
+"""
+def add(a: float, b: float) -> float:
+  """Adds two float numbers.
+
+  Args:
+    a: The first float number.
+    b: The second float number.
+
+  Returns:
+    The sum of a and b as a float.
+  """
+
+  return a + b

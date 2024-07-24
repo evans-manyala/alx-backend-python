96554140dd9043e62f797ebcc4a6883aaca0c9c1
Evans Manyala <evans.manyala@gmail.com>
2024-07-24 12:07:57 +0300

    Fixing pycodestyling errors

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -8,11 +8,11 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Converts a key and a value to a key-value tuple.
-    
+
     Args:
     k: The key as a string.
     v: The value as an integer or float.
-    
+
     Returns:
     A tuple containing the key and the square of the value.
     """

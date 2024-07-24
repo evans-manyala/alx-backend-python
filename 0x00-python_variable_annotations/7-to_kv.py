cdcd44767b9df7b7482830e450176d3ba91a197d
Evans Manyala <evans.manyala@gmail.com>
2024-07-24 12:03:39 +0300

    Adding code logic

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Function creates a tuple from a string & the square of a number.
+"""
+
+from typing import Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+    """Converts a key and a value to a key-value tuple.
+    
+    Args:
+    k: The key as a string.
+    v: The value as an integer or float.
+    
+    Returns:
+    A tuple containing the key and the square of the value.
+    """
+    return k, float(v) ** 2

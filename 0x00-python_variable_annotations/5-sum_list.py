87976034cd01a6767305bd3c74abecb90960a1b2
Evans Manyala <evans.manyala@gmail.com>
2024-07-24 11:10:28 +0300

    Adding code logic

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Function calculates sum of a list of floats"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Calculates the sum of a list of floats.
+
+    Args:
+    input_list: A list of float numbers.
+
+    Returns:
+    The sum of all elements in the input list.
+    """
+
+    return sum(input_list)

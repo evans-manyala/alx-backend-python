f3e72b16aa24812272af703da84ba6d655645e07
Evans Manyala <evans.manyala@gmail.com>
2024-07-24 10:55:00 +0300

    Adding code logic

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Converts a float to a string representation
+"""
+
+
+def to_str(n: float) -> str:
+    """Converts a float to a string representation.
+    
+    Args:
+    n: The input float.
+    
+    Returns:
+    The string representation of the float.
+    """
+    
+    return str(n)

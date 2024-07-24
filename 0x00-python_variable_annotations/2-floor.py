b363e739c75557f1712b7a48edbcba90f7f99640
Evans Manyala <evans.manyala@gmail.com>
2024-07-24 10:51:28 +0300

    Adding code logic

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Function returns the floor of a float"""
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a float.
+
+    Args:
+    n: The input float.
+
+    Returns:
+    The largest integer less than or equal to n.
+    """
+    return math.floor(n)

1b170451ca090febcf9a0acab4f14149ab7d00c8
Evans Manyala <evans.manyala@gmail.com>
2024-07-25 13:57:32 +0300

    Adding script error fix

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,23 +3,20 @@
 Function that safely gets a value
 from a dictionary with a default.
 """
-from typing import TypeVar, Dict, Any, Optional
+from typing import TypeVar, Dict, Any, Mapping, Union
 
 T = TypeVar('T')
 
-def safely_get_value(dct: Dict[Any, T], key: Any, default: Optional[T] = None) -> Optional[T]:
+def safely_get_value(dct: Mapping, key: Any, default: T = None) -> \
+        Union[Any, T]:
     """
     Safely get a value from a dictionary with a default.
 
     Args:
-        dct (Dict[Any, T]): The dictionary to get the value from.
-        key (Any): The key to look for in the dictionary.
-        default (Optional[T]): The default value to return
-        if the key is not found. Default is None.
-
-    Returns:
-        Optional[T]: The value from the dictionary
-        if the key is found, otherwise the default value.
+         dct -> dictionary
+        key -> dict key
+        default -> None
+    Return: default
     """
     if key in dct:
         return dct[key]

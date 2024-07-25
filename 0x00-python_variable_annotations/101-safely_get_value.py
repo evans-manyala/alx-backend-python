56e5f7954f261cb97b4c1f9d2dc4a534040041a7
Evans Manyala <evans.manyala@gmail.com>
2024-07-25 14:02:41 +0300

    Fixing errors

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,9 +3,10 @@
 Function that safely gets a value
 from a dictionary with a default.
 """
-from typing import TypeVar, Dict, Any, Mapping, Union
+from typing import TypeVar, Any, Mapping, Union
 
 T = TypeVar('T')
+K = TypeVar('K')
 
 def safely_get_value(dct: Mapping, key: Any, default: T = None) -> \
         Union[Any, T]:

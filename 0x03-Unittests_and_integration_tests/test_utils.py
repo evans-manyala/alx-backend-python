825350fd9b6f4e4a28236297f608a99bb371bd5d
Evans Manyala <evans.manyala@gmail.com>
2024-07-28 23:36:55 +0300

    Fixing errors

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -67,10 +67,14 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, "a_method") as mock_object:
+        with patch.object(
+            TestClass,
+            "a_method",
+            return_value=lambda: 42,
+        ) as mock_object:
             test = TestClass()
-            test.a_property()
-            test.a_property()
+            self.assertEqual(test.a_property(), 42)
+            self.assertEqual(test.a_property(), 42)
             mock_object.assert_called_once()
 
 

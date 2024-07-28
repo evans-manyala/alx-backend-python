3b24d5829878ca5715b620a932d28fefeb29295b
Evans Manyala <evans.manyala@gmail.com>
2024-07-28 21:11:38 +0300

    Adding TestMemoizeClass

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,9 +4,8 @@ Funtion to test utils.py
 """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, memoize, get_json
 from unittest.mock import patch, Mock
-from utils import get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -46,6 +45,29 @@ class TestGetJson(unittest.TestCase):
 
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+
+    class TestClass:
+        def a_method(self):
+            return 42
+
+        @memoize
+        def a_property(self):
+            return self.a_method()
+
+    @patch.object(TestClass, 'a_method', return_value=42)
+    def test_memoize(self, mock_a_method):
+        test_instance = self.TestClass()
+
+        first_call = test_instance.a_property()
+        second_call = test_instance.a_property()
+
+        mock_a_method.assert_called_once()
+
+        self.assertEqual(first_call, 42)
+        self.assertEqual(second_call, 42)
 
 
 if __name__ == "__main__":

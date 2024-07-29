4e18f1ca27278f83e5d34b37822283ab77e1dd85
Evans Manyala <evans.manyala@gmail.com>
2024-07-29 22:31:44 +0300

    Adding script

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@ Testing the client module.
 """
 import unittest
 from typing import Dict
-from unittest.mock import patch, Mock, MagicMock
+from unittest.mock import patch, Mock, MagicMock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -32,6 +32,19 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(result, expected_org)
+
+    @patch("client.GithubOrgClient.org", new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org: PropertyMock) -> None:
+        """Test that _public_repos_url returns the correct URL."""
+        test_payload = {"repos_url":
+                        "https://api.github.com/orgs/google/repos"}
+        mock_org.return_value = test_payload
+
+        client = GithubOrgClient("google")
+        result = client._public_repos_url
+
+        self.assertEqual(result, test_payload["repos_url"])
+        mock_org.assert_called_once()
 
 
 if __name__ == "__main__":
